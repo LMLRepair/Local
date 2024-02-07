fa7aea63aeab20c24db53b49d89a58dfd28f6740
LMLRepair <lookmanlookrepair@gmail.com>
2024-02-07 16:25:23 +0300

    update

--- pages/services/index.js
+++ pages/services/index.js
@@ -8,7 +8,7 @@ export default function Services() {
       <main className="p-5 bg-white">
         <section className="flex flex-col justify-center items-center p-5 bg-white">
           <h1 className="text-4xl tracking-widest">Services</h1>
-          <p className="text-md ">Select the service you are interested in</p>
+          <p className="text-md ">Select the service category you are interested in</p>
        </section>
         <article className="grid gap-5 justify-center text-center p-10">
           <figure className="border-4 border-black p-5">
