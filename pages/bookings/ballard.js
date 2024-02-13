8d25517aa8ea64833a782787328a4695bb1e0e30
LMLRepair <lookmanlookrepair@gmail.com>
2024-02-13 21:37:51 +0300

    duplicated like whats on the main branch

--- pages/bookings/ballard.js
+++ pages/bookings/ballard.js
@@ -9,12 +9,21 @@ export default function Ballard() {
 
           <iframe
             src="https://squareup.com/appointments/book/89pnd6xdrlisrg/LZ5QN88Z09J85/start"
-            style={{ border: "none" }}
-            aria-hidden="false"
             title="Square bridge for Setting Appointments"
             height="1700px"
             className="w-full mt-5"
           ></iframe>
+          {/*
+          <iframe
+            src="https://squareup.com/appointments/book/89pnd6xdrlisrg/LZ5QN88Z09J85/start"
+            style={{ border: "none" }}
+            aria-hidden="false"
+            frameborder="0"
+            title="Square bridge for Setting Appointments"
+            height="1700px"
+            className="w-full mt-5"
+          ></iframe>
+*/}
         </div>
       </div>
     </Layout>
--- pages/bookings/westseattle.js
+++ pages/bookings/westseattle.js
@@ -9,17 +9,28 @@ export default function WestSeattle() {
       <div className="container mx-auto">
         <div className="flex flex-col justify-center items-center p-10 bg-white">
           <h1 className="text-4xl mb-10 tracking-widest">
+            {" "}
             West Seattle Bookings
           </h1>
+         
+          <iframe
+            src="https://squareup.com/appointments/book/89pnd6xdrlisrg/LWQRVGS73Q18V/start"
+            title="Square bridge for Setting Appointments"
+            height="1700px"
+            className="w-full mt-5"
+          />
+         {/*
 
           <iframe
             src="https://squareup.com/appointments/book/89pnd6xdrlisrg/LWQRVGS73Q18V/start"
             style={{ border: "none" }}
             aria-hidden="false"
+            frameborder="0"
             title="Square bridge for Setting Appointments"
             height="1700px"
             className="w-full mt-5"
           ></iframe>
+  */}
         </div>
       </div>
     </Layout>
