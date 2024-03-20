3d0bb44ac57b8f2504cb07299232f49b9f551821
LMLRepair <lookmanlookrepair@gmail.com>
2024-03-20 21:38:02 +0300

    Added text to alert customers to specify their models and repairs

--- pages/bookings/seattle.js
+++ pages/bookings/seattle.js
@@ -6,6 +6,7 @@ export default function Seattle() {
       <div className="container mx-auto">
         <div className="flex flex-col justify-center items-center p-10 bg-white">
           <h1 className="text-4xl mb-10 tracking-widest"> Seattle Bookings</h1>
+          <p className="text-center">Please specify your device and the repair needed inside Appointment Notes</p>
 
           <iframe
             src="https://squareup.com/appointments/book/89pnd6xdrlisrg/LZ5QN88Z09J85/start"
--- pages/bookings/westseattle.js
+++ pages/bookings/westseattle.js
@@ -9,10 +9,9 @@ export default function WestSeattle() {
       <div className="container mx-auto">
         <div className="flex flex-col justify-center items-center p-10 bg-white">
           <h1 className="text-4xl mb-10 tracking-widest">
-            {" "}
             West Seattle Bookings
           </h1>
-         
+         <p className="text-center">Please specify your device and the repair needed inside Appointment Notes</p>
           <iframe
             src="https://squareup.com/appointments/book/89pnd6xdrlisrg/LWQRVGS73Q18V/start"
             title="Square bridge for Setting Appointments"
