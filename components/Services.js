b78bdc5f94b715b33e9de400b9808d19e07d5ca0
Lenovo <nemorashed1212@gmail.com>
2024-04-21 15:00:21 -0700

    changed services content and there icons

--- components/Services.js
+++ components/Services.js
@@ -10,7 +10,7 @@ function Services() {
       initial="hidden"
       whileInView="show"
       viewport={{ once: false, amount: 0.25 }}
-      className="flex flex-col mx-5 md:mx-24 my-24 bg-yellow-100 px-6  py-7 rounded-lg"
+      className="flex flex-col mx-5 md:mx-24 my-24  px-6  py-7 rounded-lg"
     >
       <motion.div
         variants={fadeIn("up", "tween", 0.2, 1)}
@@ -25,57 +25,54 @@ function Services() {
         variants={fadeIn("up", "tween", 0.2, 1)}
         className="flex flex-wrap justify-center items-center gap-3 mt-5"
       >
-        <div className=" flex flex-row justify-center items-center w-full md:w-1/3   bg-white px-5 py-7 radius">
-          <div className="w-14 bg-yellow-200 rounded-full p-2 mb-3">
+        <div className=" flex flex-row justify-center items-center w-full md:w-1/3   bg-gray-50 px-5 py-7 radius">
+          <div className="w-20 bg-yellow-200 rounded-full p-2 mb-3">
             <Image
-              alt="repair"
+              alt="expert"
               width={100}
               height={100}
               className="w-full"
-              src="/repair.png"
+              src="/images/services/expert.png"
             />
           </div>
           <div className="flex flex-col ml-4">
-            <h1>HIGH-QUALITY PARTS</h1>
+            <h1>Expert Electronic Repair</h1>
             <p className="text-md ">
-              LML Repair only uses high-quality OEM and aftermarket parts for
-              repairs
+              Reliable solutions for all device malfunctions.
             </p>
           </div>
         </div>
-        <div className=" flex flex-row justify-center items-center w-full md:w-1/3  bg-white px-5 py-7 radius">
-          <div className="w-14 bg-yellow-200 rounded-full p-2 mb-3">
+        <div className=" flex flex-row justify-center items-center w-full md:w-1/3  bg-gray-50 px-5 py-7 radius">
+          <div className="w-20 bg-yellow-200 rounded-full p-2 mb-3">
             <Image
-              alt="repair"
-              width={100}
-              height={100}
+              alt="diagnostic"
+              width={40}
+              height={40}
               className="w-full"
-              src="/repair.png"
+              src="/images/services/diagnostic.png"
             />
           </div>
           <div className="flex flex-col ml-4">
-            <h1>HIGH-QUALITY PARTS</h1>
+            <h1>Advanced Diagnostic Solutions</h1>
             <p className="text-md ">
-              LML Repair only uses high-quality OEM and aftermarket parts for
-              repairs
+              Precision diagnostics for troubleshooting with innovation.
             </p>
           </div>
         </div>
-        <div className=" flex flex-row justify-center items-center w-full md:w-1/3  bg-white px-5 py-7 radius">
-          <div className="w-14 bg-yellow-200 rounded-full p-2 mb-3">
+        <div className=" flex flex-row justify-center items-center w-full md:w-1/3  bg-gray-50 px-5 py-7 radius">
+          <div className="w-16 bg-yellow-200 rounded-full p-3 mb-3">
             <Image
-              alt="repair"
+              alt="price"
               width={100}
               height={100}
-              className="w-full"
-              src="/repair.png"
+              className="w-full "
+              src="/images/services/price.png"
             />
           </div>
           <div className="flex flex-col ml-4">
-            <h1>HIGH-QUALITY PARTS</h1>
+            <h1>Buyback</h1>
             <p className="text-md ">
-              LML Repair only uses high-quality OEM and aftermarket parts for
-              repairs
+              Trade-in old electronics for cash or credit
             </p>
           </div>
         </div>
@@ -86,57 +83,54 @@ function Services() {
           className="w-52"
           src="/service.png"
         />
-        <div className=" flex flex-row justify-center items-center w-full md:w-1/3  bg-white px-5 py-7 radius">
-          <div className="w-14 bg-yellow-200 rounded-full p-2 mb-3">
+        <div className=" flex flex-row justify-center items-center w-full md:w-1/3  bg-gray-50 px-5 py-7 radius">
+          <div className="w-20 bg-yellow-200 rounded-full p-2 mb-3">
             <Image
-              alt="repair"
+              alt="clean"
               width={100}
               height={100}
               className="w-full"
-              src="/repair.png"
+              src="/images/services/clean.png"
             />
           </div>
           <div className="flex flex-col ml-4">
-            <h1>HIGH-QUALITY PARTS</h1>
+            <h1>Thorough Device Cleaning</h1>
             <p className="text-md ">
-              LML Repair only uses high-quality OEM and aftermarket parts for
-              repairs
+              Revitalize gadgets with meticulous cleaning services.
             </p>
           </div>
         </div>
-        <div className=" flex flex-row justify-center items-center w-full md:w-1/3  bg-white px-5 py-7 radius">
-          <div className="w-14 bg-yellow-200 rounded-full p-2 mb-3">
+        <div className=" flex flex-row justify-center items-center w-full md:w-1/3  bg-gray-50 px-5 py-7 radius">
+          <div className="w-20 bg-yellow-200 rounded-full p-3 mb-3">
             <Image
               alt="repair"
-              width={100}
-              height={100}
+              width={80}
+              height={80}
               className="w-full"
               src="/repair.png"
             />
           </div>
           <div className="flex flex-col ml-4">
-            <h1>HIGH-QUALITY PARTS</h1>
+            <h1>Customized Repair Solutions</h1>
             <p className="text-md ">
-              LML Repair only uses high-quality OEM and aftermarket parts for
-              repairs
+              Tailor your device to match your unique style and preferences.
             </p>
           </div>
         </div>
-        <div className=" flex flex-row justify-center items-center w-full md:w-1/3  bg-white px-5 py-7 radius">
-          <div className="w-14 bg-yellow-200 rounded-full p-2 mb-3">
+        <div className=" flex flex-row justify-center items-center w-full md:w-1/3  bg-gray-50 px-5 py-7 radius">
+          <div className="w-20 bg-yellow-200 rounded-full p-2 mb-3">
             <Image
-              alt="repair"
+              alt="unlock"
               width={100}
               height={100}
               className="w-full"
-              src="/repair.png"
+              src="/images/services/unlock.png"
             />
           </div>
           <div className="flex flex-col ml-4">
-            <h1>HIGH-QUALITY PARTS</h1>
+            <h1>Seamless Unlocking Services</h1>
             <p className="text-md ">
-              LML Repair only uses high-quality OEM and aftermarket parts for
-              repairs
+              Unlock your device for expanded flexibility and functionality.
             </p>
           </div>
         </div>
